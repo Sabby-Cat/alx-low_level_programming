ea3f521db53f9b98ae029f51afae6a52be51e669
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-02-16 08:16:50 -0800

    4 5 6 7 8 9

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Description: main - print using printf
+ *
+ * return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
