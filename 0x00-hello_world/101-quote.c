b7beb736e9e6ccaa36f604ca04f29c3f06004a6d
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-02-16 08:46:10 -0800

    9

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,14 @@
-#include <stdio>
+#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - prints info
  *
- * Return: 0 (Success)
+ * Return: 1 (Success)
  */
 
 int main(void)
 {
-	write(1, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
