df2ac2a601815ea72095333b8b4bd832c9a0c11f
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-02-17 04:09:36 -0800

    12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 /**
- * main -
+ * main - 2 sets of nrs together
  *
  * Return: 0
  */
 int main(void)
 {
+	int i, j, k, l;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 58; j++)
+		{
+			for (k = 48; k < 58; k++)
+			{
+				for (l = 48; l < 58; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (i == 57 && j == 56 && k == 57 && l == 57)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
 	return (0);
 }
