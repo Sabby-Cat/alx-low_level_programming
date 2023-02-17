107a9c9b514ba2b017372e5f7713660583764ae8
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-02-17 03:16:01 -0800

    11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,42 @@
 #include <stdio.h>
 /**
- * main -
+ * main - print all unique 3 nr combinations
  *
  * Return: 0
  */
 int main(void)
 {
+	int i;
+	int j;
+	int k;
 
+	for (i = 48; i < 56; i++)
+	{
+		for (j = 49; j < 57; j++)
+		{
+			for (k = 50; k < 58; k++)
+			{
+				if (i >= j || j >= k)
+				{
+					;
+				}
+				else
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i == 55 && j == 56 && k == 57)
+					{
+						putchar('\n');
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
 	return (0);
 }
