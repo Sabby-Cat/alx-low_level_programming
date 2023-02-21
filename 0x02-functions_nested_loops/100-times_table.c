88add14fa384f2a19314c3a55cb5bbff1ec48f73
Sabby-Cat <122835289+Sabby-Cat@users.noreply.github.com>
2023-02-21 17:39:00 +0200

    12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,39 +3,28 @@
  * print_times_table - print times table of give nr(between 0 and 15)
  * @n: n times table
  */
-void print_times_table(int n)
-{
+void print_times_table(int n) {
 	int i, j, a;
 
-	if (n <= 15 && n >= 0)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
+	if (n <= 15 && n >= 0) {
+		for (i = 0; i <= n; i++) {
+			for (j = 0; j <= n; j++) {
 				a = j * i;
-				if (j == 0)
-				{
+				if (j == 0) {
 					_putchar(a + '0');
-				}
-				else if (a < 10)
-				{
+				}else if (a < 10) {
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(a + '0');
-				}
-				else if (a >= 10 && a < 100)
-				{
+				}else if (a >= 10 && a < 100) {
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((a / 10) + '0');
 					_putchar((a % 10) + '0');
-				}
-				else
-				{
+				}else {
 					_putchar(',');
 					_putchar(' ');
 					_putchar((a / 100) + '0');
