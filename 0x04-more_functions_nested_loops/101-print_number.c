cec6fd70366645422759e9c335b0b58c91812462
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-02-23 06:38:02 -0800

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -16,26 +16,27 @@ void print_number(int n)
 	{
 		m = n;
 	}
-	if (m < 10)
-	{
-		_putchar(m + '0');
-	}
 	if (m >= 10 && m < 100)
 	{
 		_putchar((m / 10) + '0');
-		_putchar((m % 10) + '0');
 	}
 	if (m >= 100 && m < 1000)
 	{
 		_putchar((m / 100) + '0');
 		_putchar((m / 10) % 10 + '0');
-		_putchar((m % 10) + '0');
 	}
 	if (m >= 1000 && m < 10000)
 	{
 		_putchar((m / 1000) + '0');
 		_putchar((m / 100) % 10 + '0');
 		_putchar((m / 10) % 10 + '0');
-		_putchar((m % 10) + '0');
 	}
+	if (m >= 10000 && m < 100000)
+	{
+		_putchar((m / 10000) + '0');
+		_putchar((m / 1000) % 10 + '0');
+		_putchar((m / 100) % 10 + '0');
+		_putchar((m / 10) % 10 + '0');
+	}
+	_putchar((m % 10) + '0');
 }
