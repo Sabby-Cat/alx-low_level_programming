4462a2b867730be2fb7dd9943e3f3eb780cefb05
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-02-27 09:23:17 -0800

    10, 11

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 /**
- * main -
- * Return:
+ * main - generates valid passwords for 101-crackme
+ * Return: 0
  */
 int main(void)
 {
+	int pass;
+	char r;
+
+	srand(time(NULL));
+	pass = 0;
+	while (pass <= 2645)
+	{
+		r = rand() % 128;
+		pass += r;
+		putchar(r);
+	}
+	putchar(2772 - sum);
+	return (0);
 }
