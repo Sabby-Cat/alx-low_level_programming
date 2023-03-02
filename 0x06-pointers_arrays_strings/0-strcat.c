bf4e4d57987d42aee1824d90a29f2e8329181537
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-03-01 23:18:11 -0800

    0 redo

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,7 +12,7 @@ char *_strcat(char *dest, char *src)
 
 	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest[strlen(dest) + i] = src[i];
+		dest[strlen(dest) - 1 + i] = src[i];
 	}
 	dest[strlen(dest) + i] = '\0';
 	return (dest);
