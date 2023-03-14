676f39f8f6edeb8a65bfb99cd03fc558edb58015
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-03-14 05:37:21 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 	int **ret;
 	int i, j;
 
-	if (width < 0 || height < 0)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 
 	ret = malloc(height * sizeof(int *));
