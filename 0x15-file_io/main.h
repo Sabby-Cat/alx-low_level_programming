1a94448669b113b5dcd8c5032bf311bdbb0a548d
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-04-10 11:20:03 -0700

    4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
