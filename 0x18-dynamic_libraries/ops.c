19559faedc9f1a59964bedc23186cc81778224c2
Sabrina Beukes <sabrinabeukes@gmail.com>
2023-11-13 01:58:53 -0800

    attempt

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,65 @@
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+
+/**
+* add - adds two ints
+* @i: first int
+* @k: second int
+* Return: answer
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub - subtracts two ints
+* @i: first int
+* @k: second int
+* Return: answer
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul - multiplies two ints
+* @i: first int
+* @k: second int
+* Return: answer
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div - divideds two ints
+* @i: first int
+* @k: second int
+* Return: answer
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+  mod - finds the modulus of two ints
+  @i: first int
+  @k: second int
+  Return: answer
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
