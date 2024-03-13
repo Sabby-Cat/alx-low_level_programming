149d0f3d3b91f3ce285466b754d95563a4b73d54
Sabby-Cat <122835289+Sabby-Cat@users.noreply.github.com>
2024-03-13 23:22:25 +0200

    fix

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -22,17 +22,17 @@ listint_t *jump_list(listint_t *list, size_t size, int value)
 		prev = list;
 		i++;
 		idx = i * j;
-		while (list->next && list->idx < idx)
+		while (list->next && list->index < idx)
 			list = list->next;
-		if (list->next == NULL && idx != list->idx)
-			idx = list->idx;
+		if (list->next == NULL && idx != list->index)
+			idx = list->index;
 		printf("Value checked at index [%d] = [%d]\n", (int)idx, list->n);
 	} while (idx < size && list->next && list->n < value);
 	printf("Value found between indexes ");
-	printf("[%d] and [%d]\n", (int)prev->idx, (int)list->idx);
-	for (; prev && prev->idx <= list->idx; prev = prev->next)
+	printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
+	for (; prev && prev->index <= list->index; prev = prev->next)
 	{
-		printf("Value checked at index [%d] = [%d]\n", (int)prev->idx, prev->n);
+		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
 		if (prev->n == value)
 			return (prev);
 	}
